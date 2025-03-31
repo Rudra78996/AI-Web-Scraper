c8678f41a18d2b91b557fe8385c8b8e33aa892a9
Rudra Pratap Singh <rudra568433@gmail.com>
2025-03-31 12:06:48 +0530

    scraper v-2

--- scraper.js
+++ scraper.js
@@ -7,7 +7,7 @@ async function scraper(url) {
     try {
         const browser = await puppeteer.launch({
             headless: "new",
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || (await import("puppeteer")).executablePath(),
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
@@ -15,7 +15,7 @@ async function scraper(url) {
                 "--disable-gpu",
                 "--disable-features=IsolateOrigins,site-per-process"
             ]
-        }); 
+        });
 
         const page = await browser.newPage();
         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
